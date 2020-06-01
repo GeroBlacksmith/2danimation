03ba2ce07b642e85f6c798b72f6ea80d9cc08121
geroblksmz <gerardoherrera87@yahoo.com.ar>
2020-06-01 20:50:01 -0300

    Only commit

---
+++ Assets/Aiden.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aiden : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKey("left"))
+        {
+            gameObject.GetComponent<Animator>().SetBool("moving", true);
+            gameObject.GetComponent<SpriteRenderer>().flipX = true;
+        }
+        if (Input.GetKey("right"))
+        {
+            gameObject.GetComponent<SpriteRenderer>().flipX = false;
+            gameObject.GetComponent<Animator>().SetBool("moving", true);
+        }
+        if (!Input.GetKey("right") && !Input.GetKey("left"))
+        {
+            gameObject.GetComponent<Animator>().SetBool("moving", false);
+        }
+    }
+}
